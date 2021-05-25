708ae835e35b2542f3fc8bccb1c87021708b6315
Corine <corine@localhost.localdomain>
2021-05-25 18:47:11 +0100

    not auto-generate CSS, JavaScript, and Helper files with the rails g command

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,19 @@
 require_relative 'boot'
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Task2
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      # The description in these two lines creates a Settings that is not automatically generated.
+      g.assets false
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
