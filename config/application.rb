0da7afa38037a91c3829a570e137cd137f27df3a
Corine <corine@localhost.localdomain>
2021-05-27 15:04:34 +0100

    Internationalisation des messages, models

--- config/application.rb
+++ config/application.rb
@@ -4,15 +4,15 @@ Bundler.require(*Rails.groups)
 
 module Task2
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Paris'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :fr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators do |g|
       # The description in these two lines creates a Settings that is not automatically generated.
-      g.assets false
-      g.helper false
+		g.assets false
+		g.helper false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
